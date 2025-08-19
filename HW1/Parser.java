181bc30a02e1990ce37e092b424893a32b0dde14
Diya Lakhani <diyalakhani@Diyas-MBP-2.lan>
2025-08-18 18:20:39 -0700

    add test cases

--- HW1/Parser.java
+++ HW1/Parser.java
@@ -1,11 +1,24 @@
 import java.util.*;
 
+/**
+ * Parser.java
+ *
+ * Description: Recursive descent parser for LL(1) grammar.
+ *
+ * @author Diya Lakhani
+ * @version 1.0
+ * @since 2025-08-18
+ */
+
+
 public class Parser {
 
     enum TokenType {NUM, DOLLAR, PLUS, MINUS, PP, MM, LPAREN, RPAREN, EOF, HASH}
 
-    // Token Class
-    // Container for a single token [++, --, +, -, 0-9, $, (, )]
+    /*
+     * Token Class
+     * Container for a single token [++, --, +, -, 0-9, $, (, )]
+     */
     static class Token {
         TokenType type;
         String text;
@@ -16,6 +29,9 @@ public class Parser {
         }
     }
 
+    /*
+     * Scanner for input syntax
+     */
     static class LexScanner {
         String input;
         int currIdx;
@@ -119,7 +135,9 @@ public class Parser {
     }
 
 
-
+    /*
+     * Class to implement recursive descent parser.
+     */
     static class Parse {
 
         private final LexScanner lexsc;
@@ -145,10 +163,7 @@ public class Parser {
 
         // E -> AE'
         void E() {
-            // TODO
-            System.out.println("In E() " + curr.text);
-
-            if (curr.type == TokenType.HASH) {
+            while (curr.type == TokenType.HASH) {
                 curr = lexsc.nextToken();
             }
             if (curr.type != TokenType.EOF) {
@@ -158,19 +173,27 @@ public class Parser {
         }
 
         // A -> N | L | IE | (E)
-        void A() {
-            // TODO
-            System.out.println("In A() " + curr.text);
+        String A() {
+            while (curr.type == TokenType.HASH) {
+                curr = lexsc.nextToken();
+            }
 
             if (curr.type == TokenType.NUM) {
+                parsedString.append(curr.text + " ");
                 N();
             }
             else if (curr.type == TokenType.DOLLAR) {
                 L();
             }
-            else if (curr.type == TokenType.PP || curr.type == TokenType.MM) {
+            else if (curr.type == TokenType.PP) {
                 I();
-                E();
+                A();
+                parsedString.append("++_ ");
+            }
+            else if (curr.type == TokenType.MM) {
+                I();
+                A();
+                parsedString.append("--_ ");
             }
             else if (curr.type == TokenType.LPAREN) {
                 curr = lexsc.nextToken();
@@ -182,24 +205,30 @@ public class Parser {
                     throw new RuntimeException("Unexpected token in (E) in A()!");
                 }
             }
+            else if (curr.type == TokenType.EOF) {
+                return "";
+            }
             else {
                 throw new RuntimeException("Unexpected token in A()!");
             }
+
+            return "";
 
         }
 
         // E' -> IEE' | BEE' | empty
         void Eprime() {
-            // TODO
-            System.out.println("In Eprime() " + curr.text);
-
             if (curr.type == TokenType.PP || curr.type == TokenType.MM) {
+                String operator = curr.text;
                 I();
+                parsedString.append("_" + operator + " ");
                 Eprime();
             }
             else if (curr.type == TokenType.PLUS || curr.type == TokenType.MINUS) {
+                String operator = curr.text;
                 B();
-                E();
+                A();
+                parsedString.append(operator + " ");
                 Eprime();
             }
             else if (curr.type == TokenType.DOLLAR ||
@@ -208,7 +237,8 @@ public class Parser {
                     curr.type == TokenType.MM ||
                     curr.type == TokenType.LPAREN) {
                 // String concatenation
-                E();
+                A();
+                parsedString.append("_ ");
                 Eprime();
             }
         }
@@ -216,9 +246,6 @@ public class Parser {
 
         // I -> ++ | --
         void I() {
-            // TODO
-            System.out.println("In I() " + curr.text);
-
             if (curr.type == TokenType.PP || curr.type == TokenType.MM) {
                 curr = lexsc.nextToken();
             }
@@ -230,12 +257,10 @@ public class Parser {
 
         // L -> $E
         void L() {
-            // TODO
-            System.out.println("In L() " + curr.text);
-
             if (curr.type == TokenType.DOLLAR) {
                 curr = lexsc.nextToken();
-                E();
+                A();
+                parsedString.append("$ ");
             }
             else {
                 throw new RuntimeException("Unexpected token in L()!");
@@ -245,24 +270,14 @@ public class Parser {
 
         // B -> + | - |
         void B() {
-            // TODO
-            System.out.println("In B() " + curr.text);
-
             if (curr.type == TokenType.PLUS || curr.type == TokenType.MINUS) {
                 curr = lexsc.nextToken();
             }
-            // else {
-            //     // TODO: String concat
-            //     curr = lexsc.nextToken();
-            // }
         }
 
 
         // N -> [0-9]
         void N() {
-            // TODO
-            System.out.println("In N() " + curr.text);
-
             if (curr.type == TokenType.NUM) {
                 curr = lexsc.nextToken();
             }
@@ -285,38 +300,59 @@ public class Parser {
 
         // TEST CASE 1
 
-        // String input = "(1 2)";
-        // lexsc = new LexScanner(input);
-        // Parse parser = new Parse(lexsc);
-        // parser.parse();
+        String input1 = "(1 2)";
+        System.out.println("Input: " + input1);
+        LexScanner lexsc1 = new LexScanner(input1);
+        Parse parser1 = new Parse(lexsc1);
+        StringBuilder parsedStr1 = parser1.parse();
+        System.out.println("Parsed Str: " + parsedStr1);
+
 
         // TEST CASE 2
 
-        // String input = "$  1 + \n (1 -++$2) $ #sfnonorneo \n 3";
-        // lexsc = new LexScanner(input);
-        // Parse parser = new Parse(lexsc);
-        // parser.parse();
+        String input2 = "$  1 + \n (1 -++$2) $ #some strange comment \n 3";
+        System.out.println("Input: " + input2);
+        LexScanner lexsc2 = new LexScanner(input2);
+        Parse parser2 = new Parse(lexsc2);
+        StringBuilder parsedStr2 = parser2.parse();
+        System.out.println("Parsed Str: " + parsedStr2);
 
         // TEST CASE 3
 
-        // String input = "$ $ 1 ++ ++ - $2";
-        // lexsc = new LexScanner(input);
-        // Parse parser = new Parse(lexsc);
-        // parser.parse();
+        String input3 = "$ $ 1 ++ ++ - $2";
+        System.out.println("Input: " + input3);
+        LexScanner lexsc3 = new LexScanner(input3);
+        Parse parser3 = new Parse(lexsc3);
+        StringBuilder parsedStr3 = parser3.parse();
+        System.out.println("Parsed Str: " + parsedStr3);
 
         // TEST CASE 4
 
-        // String input = "1 ++ ++ - $2";
-        // lexsc = new LexScanner(input);
-        // Parse parser = new Parse(lexsc);
-        // parser.parse();
+        String input4 = "1 ++ ++ - $2";
+        System.out.println("Input: " + input4);
+        LexScanner lexsc4 = new LexScanner(input4);
+        Parse parser4 = new Parse(lexsc4);
+        StringBuilder parsedStr4 = parser4.parse();
+        System.out.println("Parsed Str: " + parsedStr4);
 
-        // TEST CASE 4
+        // TEST CASE 5
 
-        String input = "$ 1 2";
-        lexsc = new LexScanner(input);
-        Parse parser = new Parse(lexsc);
-        parser.parse();
+        String input5 = "$ ++ 1 - 2";
+        System.out.println("Input: " + input5);
+        LexScanner lexsc5 = new LexScanner(input5);
+        Parse parser5 = new Parse(lexsc5);
+        StringBuilder parsedStr5 = parser5.parse();
+        System.out.println("Parsed Str: " + parsedStr5);
+
+
+        // TEST CASE 6
+
+        String input6 = "++1++";
+        System.out.println("Input: " + input6);
+        LexScanner lexsc6 = new LexScanner(input6);
+        Parse parser6 = new Parse(lexsc6);
+        StringBuilder parsedStr6 = parser6.parse();
+        System.out.println("Parsed Str: " + parsedStr6);
     }
 
 }
